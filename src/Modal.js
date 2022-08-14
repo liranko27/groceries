aca59507ddbc0cc9c2693736f89e8b640f62d41c
liranko27 <liranko13@gmail.com>
2022-08-14 13:06:09 +0300

    build modal with close and reset function

---
+++ src/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Item from './Item'
+import './styles/modal.css'
+
+function Modal({ items, close }) {
+    return (
+        <div className='Modal'>
+            <h1>Summery</h1>
+            <ul>
+                {items.map(item => <Item item={item} />)}
+            </ul>
+            <button onClick={() => close(false)}>Close</button>
+        </div>
+    )
+}
+
+export default Modal
